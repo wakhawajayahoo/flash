8c37b25a0fcb31b9674bd6a25589fbe6bb588982
Anwender <Anwender@Anwender-PC>
2020-10-02 19:20:51 +0500

    first Commit
    uploading all files

---
+++ ProductionUnit/src/Machine.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class Machine {
+	
+	private int Asset_price;
+	private Date purchase_date;
+	private int depriciation_rate;
+	private Date slavage_date;
+	private int elevator_id;
+	private int Machine_id;
+	private int Motor_Hp;
+	private int intake_Machineid;
+	private int outtake_Machineid;
+	
+}
